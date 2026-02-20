951862ecfb9864008a04633fecb6c45f2fbd841e
Cyrille Bernier <cyber18@ulaval.ca>
2026-02-20 16:16:56 -0500

    élégance des on_changed. Relim des sliders

--- Graphique.py
+++ Graphique.py
@@ -15,15 +15,15 @@ premier_tableau = System(50, 20, 10, 1000, 10, 0.1, 0.5)
 r = premier_tableau.affichage()
 fig, line1 = r[0], r[1]
 fig.subplots_adjust(left=0.25, bottom=0.10)
-slider_force, slider_omega, slider_masse, slider_raideur, slider_amortissement, slider_x_0, slider_v_0 = gen_sliders(
+sliders = slider_force, slider_omega, slider_masse, slider_raideur, slider_amortissement, slider_x_0, slider_v_0 = gen_sliders(
     r[0],
     ["F", 3, 1000, 200],
     ["w", 0, 100, 20],
-    ["M", 2, 100, 10],
+    ["M", 2, 50, 10],
     ["K", 100, 10000, 1000],
-    ["C", 3, 200, 10],
+    ["C", 3, 100, 10],
     ["x_0", 0, 2, 0.1],
-    ["v_0", 0, 1, 0.5],
+    ["v_0", 0, 1, 0.5]
 )
 def update(val):                            # Fonction copiée depuis Claude.
     nouveau_systeme = System(
@@ -46,13 +46,9 @@ def update(val):                            # Fonction copiée depuis Claude.
     ax.autoscale_view()
     r[0].canvas.draw_idle()  # redessiner la figure    
 
-slider_force.on_changed(update)
-slider_omega.on_changed(update)
-slider_masse.on_changed(update)
-slider_raideur.on_changed(update)
-slider_amortissement.on_changed(update)
-slider_x_0.on_changed(update)
-slider_v_0.on_changed(update)
+
+for sl in sliders:
+    sl.on_changed(update)
 
     
 plt.show()
