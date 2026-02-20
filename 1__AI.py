68819f9fcc3724c1bd0616bd23cbf038fa80411e
Cyrille Bernier <cyber18@ulaval.ca>
2026-02-20 12:12:25 -0500

    toc toc, qui est là ???

--- 1__AI.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as anim
-
-help(anim)
---
+++ test.py
@@ -0,0 +1,10 @@
+import sklearn
+# from ucimlrepo import fetch_ucirepo             # Pourquoi ? Je ne le mets pas, car concernait le loading de DATASETS.
+from itertools import product                   # Pourquoi ?
+
+import numpy as np
+import matplotlib.pyplot as plt             
+
+# fonction_mystere, je l'ai laissée tomber. C'était juste pour le tutoriel. Donc inutle.
+# Tous les imports utiles sont donc effectifs.
+
